22294d8fde264857d294807e3328239e947af6cd
Robert Hardy <rhardy1@dcsdk12.org>
2022-08-23 16:39:14 -0600

    Finished guessing game

--- scripts/guessing-game.js
+++ scripts/guessing-game.js
@@ -6,13 +6,30 @@ console.log(button);
 
 button?.addEventListener("click", function () {
 	// console.log("Clicked!");
-	let answer = 7;
+	// random returns a number between 0 and 1
+	// multiply it by 10 and add then get the whole
+	// number using floor.
+	let answer = Math.floor(Math.random() * 10 + 1);
+	let numberOfGuesses = 3;
+	let usedGuesses = 1;
+	let isGameOver = false;
 
-	let guess = Number(prompt("Pick a number between 1 and 10"));
+	while (isGameOver == false) {
+		let guess = Number(prompt("Pick a number between 1 and 10"));
+		usedGuesses = usedGuesses + 1;
 
-	if (answer == guess) {
-		alert("You won!");
+		if (answer == guess) {
+			alert("You won!");
+			isGameOver = true;
+		} else if (usedGuesses > numberOfGuesses) {
+			alert("You lose! No more guesses!");
+			isGameOver = true;
+		} else {
+			if (guess < answer) {
+				alert("Too low! Guess again.");
+			} else {
+				alert("Too high! Guess again.");
+			}
+		}
 	}
-
-	console.log(guess);
 });
