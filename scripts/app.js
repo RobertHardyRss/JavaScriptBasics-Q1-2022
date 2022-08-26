062ed26119932d50d58ec5a8323b29b9310e96bc
Robert Hardy <rhardy1@dcsdk12.org>
2022-08-26 16:30:04 -0600

    Add collision detection

--- scripts/app.js
+++ scripts/app.js
@@ -12,12 +12,26 @@ ctx.fillStyle = "red";
 let x = 0;
 let y = 0;
 
+const width = 100;
+const height = 50;
+
+let xDir = 1;
+let yDir = 1;
+
 const animate = function () {
 	ctx?.clearRect(0, 0, canvas.width, canvas.height);
 
-	ctx?.fillRect(x, y, 100, 50);
-	x = x + 1;
-	y = y + 1;
+	if (x < 0 || x + width > canvas.width) {
+		xDir = xDir * -1;
+	}
+
+	if (y < 0 || y + height > canvas.height) {
+		yDir = yDir * -1;
+	}
+
+	ctx?.fillRect(x, y, width, height);
+	x = x + xDir;
+	y = y + yDir;
 	requestAnimationFrame(animate);
 };
 
